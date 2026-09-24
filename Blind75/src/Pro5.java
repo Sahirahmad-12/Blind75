c064e0958662dd706b5d23734608021051b32535
Sahir Ahmad <shrahmad2612@gmail.com>
2026-09-24 12:59:06 +0530

    Add Pro5 solution

---
+++ Blind75/src/Pro5.java
@@ -0,0 +1,19 @@
+//Leetcode 53. Maximum Subarray
+
+public class Pro5 {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int MaxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if(currentSum + nums[i]>nums[i]){
+                currentSum+=nums[i];
+            }else {
+                currentSum = nums[i];
+            }
+            MaxSum = Math.max(MaxSum, currentSum);
+        }
+        return MaxSum;
+    }
+    public static void main(String[] args) {
+    }
+}

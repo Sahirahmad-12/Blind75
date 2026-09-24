640c6901f7faf45dc87cc97c8fa25881f97726c3
Sahir Ahmad <shrahmad2612@gmail.com>
2026-09-24 12:20:41 +0530

    Update Pro4 solution

--- Blind75/src/Pro4.java
+++ Blind75/src/Pro4.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Pro4 {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int answer[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+            for (int j = 0; j < n; j++) {
+                if(i != j){
+                    product = product*nums[j];
+                }
+            }
+            answer[i] = product;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+    }
 }
